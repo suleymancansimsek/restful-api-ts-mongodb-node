46ff4ca4473f1446471a62bcdf3a311661b6543f
suleymancansimsek <suleymancansimsek@gmail.com>
2023-12-24 23:17:14 +0300

    mongodb connection

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,11 @@ import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+
+dotenv.config();
 
 const app = express();
 
@@ -20,3 +25,8 @@ const server = http.createServer(app);
 server.listen(8080, ()=>{
     console.log('Server running on http://localhost:8080/');
 });
+
+
+mongoose.Promise = Promise;
+mongoose.connect(process.env.MONGO_URL);
+mongoose.connection.on('error', (error: Error) => console.log(error)); 
